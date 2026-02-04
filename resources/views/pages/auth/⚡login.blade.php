5c76d6e900364f7153c7d86094b0286914316827
Luis Fernando Franco <luisferfranco@gmail.com>
2026-02-04 00:26:52 -0600

    Formularios de Auth

---
+++ resources/views/pages/auth/⚡login.blade.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\User;
+use Mary\Traits\Toast;
+use Livewire\Component;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Validate;
+
+new
+#[Layout('layouts.empty')]
+class extends Component
+{
+  use Toast;
+
+  public $email;
+  public $password;
+
+  public function login() {
+    $this->validate([
+      'email' => 'required|email',
+      'password' => 'required|string|min:8',
+    ]);
+
+    if (!auth()->attempt(['email' => $this->email, 'password' => $this->password])) {
+      return $this->error('Credenciales inválidas.');
+    }
+
+    // Redirigir o mostrar un mensaje de éxito
+    $this->success('Inicio de sesión exitoso.');
+    return $this->redirectRoute('dashboard');
+  }
+
+};
+?>
+
+<div class="bg-base-100 md:rounded relative pt-16 pb-4 px-4 w-full md:max-w-3xl">
+  <img src="/img/sblogo.svg" alt="SB Logo" class="absolute left-1/2 transform -translate-x-1/2 w-64 h-auto mx-auto mb-4 -top-32" />
+  <x-form wire:submit='login'>
+    <x-input label="Email" type="email" wire:model='email' class="outline-none!" inline placeholder="Email" />
+    <x-input label="Contraseña" type="password" wire:model='password' class="outline-none!" inline placeholder="Contraseña" />
+    <div class="flex gap-2 justify-between items-center">
+      <x-button type="submit" class="btn-primary">Iniciar Sesión</x-button>
+      <a href="{{ route('register') }}" class="text-sm text-base-content/70 hover:text-base-content transition">¿No tienes una cuenta? Regístrate</a>
+    </div>
+  </x-form>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,8 @@
 <?php
 
+Route::livewire('/register', 'pages::auth.register')->name('register');
+Route::livewire('/login', 'pages::auth.login')->name('login');
+Route::livewire('/dashboard', 'pages::dashboard')->name('dashboard');
+
 Route::livewire('/', 'pages::users.index');
+
